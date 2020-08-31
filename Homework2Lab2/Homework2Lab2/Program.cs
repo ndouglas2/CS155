0412e6b6fc2864216da07a7517809af97a16d12d
Nate Douglas <nate.douglas2@gmail.com>
2020-08-31 11:47:44 -0700

    Arcade tickets lab

---
+++ Homework2Lab2/Homework2Lab2/Program.cs
@@ -0,0 +1,32 @@
+﻿/// Chapter No. 2		Exercise No. 2
+/// File Name: Homework2Lab2.cs
+/// @author: Nate Douglas
+/// Date:  August 31, 2020
+///
+/// Problem Statement: Calculate the number of candy bars and gumballs one can purchase with tickets. 
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt the user for number of tickets won
+/// 3) Calculate how many candy bars can be purchased with tickets. 
+/// 4) Use the remainder to determine how many gumballs can then be purchased. 
+/// 5) Print results to screen
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! Please enter the number of tickets you have won");
+        }
+    }
+}
