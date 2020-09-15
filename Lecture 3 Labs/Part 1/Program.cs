2ac7f0e15ce48da7f818a553b1a29edc74724854
Nate Douglas <nate.douglas2@gmail.com>
2020-09-14 17:38:41 -0700

    Created assignment

---
+++ Lecture 3 Labs/Part 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
