1aa4377bd72f21a92a6a9c3b82daf05ef18551a6
Nate Douglas <nate.douglas2@gmail.com>
2020-09-20 16:40:40 -0700

    Finished Homework 3

---
+++ Homework 3/Part 1/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿/// Chapter No. 3		Exercise No. 1
+/// File Name: Homework3Part1.cs
+/// @author: Nate Douglas
+/// Date:  September 20, 2020
+///
+/// Problem Statement: Write a program that takes in a loan principal and monthly payment, and calculates the interest and remaining balance.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Have two text boxes displayed that ask for the principal and the payment being offered.
+/// 2) Take those values and input them into an interest equation and a new balance equation
+/// 3) Output all of this in a message box on the screen. 
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Part_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public static double INTEREST_RATE = .0639;
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //Grabs entered loan balance from first text box
+            double loanBalance = 0;
+            loanBalance = Convert.ToInt32(LoanBalance.Text);
+
+            //Uses entered loan balance to determine how much interest will be added to the payment
+            double paymentInterest = (INTEREST_RATE / 12) * loanBalance;
+
+            //Grabs entered monthly payment from the second text box. 
+            double monthlyPayment = 0;
+            monthlyPayment = Convert.ToInt32(PaymentAmount.Text);
+
+            //Determines the remaining balance based on the entered balance, the payment and the interest
+            double newBalance = loanBalance - (monthlyPayment - paymentInterest);
+
+            //Outputs all of this to a message box on the screen
+            MessageBox.Show("Your Current loan Balance is: $" + loanBalance + Environment.NewLine + "The current interest added this month is: $" + paymentInterest + 
+                Environment.NewLine + "To confirm, you are paying: $" + monthlyPayment + " this month." + Environment.NewLine + "As such, your new loan balance is: $" + newBalance);
+        }
+    }
+}
---
+++ Homework 3/Part 2/Program.cs
@@ -0,0 +1,67 @@
+﻿/// Chapter No. 3		Exercise No. 2
+/// File Name: Homework3Part2.cs
+/// @author: Nate Douglas
+/// Date:  September 20, 2020
+///
+/// Problem Statement: Create a vending machine program that takes in a dollar and outputs the change given in coins.
+/// 
+/// 
+/// Overall Plan:
+/// 1) Print an initial welcoming message to the screen
+/// 2) Prompt user if the lamp is plugged in
+/// 3) If yes, prompt user if the bulb is burned out
+/// 4) If no, tell user the lamp may need to be repaired.  
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Part_2
+{
+    class Program
+    {
+        public static int MONEY_GIVEN = 100;
+        static void Main(string[] args)
+        {
+            string input = "";
+            int itemCost = 0;
+            int change = 0;
+            int quarters = 0;
+            int dimes = 0;
+            int nickles = 0;
+            int remainder = 0;
+            
+            Console.WriteLine("Welcome to Vendotron 5000! Please note I only accept a single dollar at a time.");
+            Console.WriteLine();
+            Console.WriteLine("Please input the cost of your item in number of cents in increments of 5 without the decimal. So .25 = 25 and $1 = 100: ");
+            input = Console.ReadLine();
+            itemCost = Convert.ToInt32(input);
+            change = MONEY_GIVEN - itemCost;
+
+            if (itemCost >= 25 && itemCost <= 100)
+            {
+                quarters = change / 25;
+                remainder = change % 25;
+                dimes = remainder / 10;
+                remainder = remainder % 10;
+                nickles = remainder / 5;
+
+                Console.WriteLine("Your item cost " + itemCost + " cents and as you gave me a dollar, your change is: ");
+                Console.WriteLine();
+                Console.WriteLine(quarters + " Quarters.");
+                Console.WriteLine();
+                Console.WriteLine(dimes + " Dimes.");
+                Console.WriteLine();
+                Console.WriteLine(nickles + " Nickles.");
+            }
+            else
+            {
+                Console.WriteLine("I am sorry, that is not a valid cost. Please try again. Goodbye.");
+                Console.ReadLine();
+            }
+        }
+    }
+}
