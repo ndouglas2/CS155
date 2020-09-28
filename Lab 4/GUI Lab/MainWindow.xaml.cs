831e533670c5b4a02045641c86f5c594d92693f0
Nate Douglas <nate.douglas2@gmail.com>
2020-09-28 15:01:23 -0700

    Completed GUI Lab. Part 1 Is lecture examples.

---
+++ Lab 4/GUI Lab/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GUI_Lab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculatebttn_Click(object sender, RoutedEventArgs e)
+        {
+            int speedLimit = 0;
+            int userSpeed = 0;
+            int ticketCost = 0;
+
+            speedLimit = Convert.ToInt32(SLtxtbox.Text);
+            userSpeed = Convert.ToInt32(InputSpeedtxtbox.Text);
+
+            
+
+            if (userSpeed <= speedLimit)
+            {
+                tempCanvas.Background = Brushes.Green;
+                Penaltytxtbox.Text = "Congrats! You were going the speed limit.";
+            }
+            else if (userSpeed >= (speedLimit + 25))
+            {
+                tempCanvas.Background = Brushes.Red;
+                ticketCost = 60 + (7 * (userSpeed - speedLimit)) + 250;
+                Penaltytxtbox.Text = "Wow you were going fast! Sorry, but that is a $" + ticketCost + " ticket.";
+            }
+            else
+            {
+                tempCanvas.Background = Brushes.Yellow;
+                ticketCost = 60 + (7 * (userSpeed - speedLimit));
+                Penaltytxtbox.Text = "Well I guess you were going fast but it could have been worse. You will be issued a ticket of $" + ticketCost;
+            }
+
+        }
+    }
+}
---
+++ Lab 4/Part 1/Program.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.VisualBasic.CompilerServices;
+using System;
+
+namespace Part_1
+{
+    class Program
+    {
+
+        public static int SPEED_LIMIT = 65;
+        static void Main(string[] args)
+        {
+            /*int currentSpeed = 0;
+            int ticketCost = 0;
+
+            Console.WriteLine("Please enter the speed you were going: ");
+            currentSpeed = Convert.ToInt32(Console.ReadLine());
+
+            if (currentSpeed <= SPEED_LIMIT)
+            {
+                Console.WriteLine("Congrats! You were going the speed limit.");
+            }
+            else if (currentSpeed >= (SPEED_LIMIT + 25))
+            {
+                ticketCost = 60 + (7 * (currentSpeed - SPEED_LIMIT)) + 250;
+                Console.WriteLine("Wow you were going fast! Sorry, but that is a $" + ticketCost + " ticket.");
+            }
+            else
+            {
+                ticketCost = 60 + (7 * (currentSpeed - SPEED_LIMIT));
+                Console.WriteLine("Well I guess you were going fast but it could have been worse. You will be issued a ticket of $" + ticketCost);
+            }
+            */
+            //Loop example: will help with homework
+            int numberA = 0, numberB= 0, numberC = 0, numberD = 0, numberF = 0, total = 0, grade = 0;
+            double percentA = 0.0, percentB = 0.0, percentC = 0.0, percentD = 0.0, percentF = 0.0;
+
+            
+            while (grade >= 0)
+            {
+                Console.WriteLine("Please enter a grade, When finished, please enter -1: ");
+                grade = Convert.ToInt32(Console.ReadLine());
+
+                if (grade >= 90 && grade <= 100)
+                {
+                    numberA++;
+                    total++;
+                }
+                else if (grade >= 80 && grade <= 89)
+                {
+                    numberB++;
+                    total++;
+                }
+                else if (grade >= 70 && grade <= 79)
+                {
+                    numberC++;
+                    total++;
+                }
+                else if (grade >= 60 && grade <= 69)
+                {
+                    numberD++;
+                    total++;
+                }
+                else if (grade >= 0 && grade <= 59)
+                {
+                    numberF++;
+                    total++;
+                }
+
+                
+                Console.WriteLine();
+            }
+
+            percentA = (numberA / (double)total) *100;
+            percentB = (numberB / (double)total) *100;
+            percentC = (numberC / (double)total) *100;
+            percentD = (numberD / (double)total) *100;
+            percentF = (numberF / (double)total) *100;
+
+            Console.WriteLine("Total Grades: " + total);
+            Console.WriteLine();
+            Console.WriteLine("Number of A's = " + numberA + " Which is " + percentA.ToString("0.00") + "% of the total grades.");
+            Console.WriteLine();
+            Console.WriteLine("Number of B's = " + numberB + " Which is " + percentB.ToString("0.00") + "% of the total grades.");
+            Console.WriteLine();
+            Console.WriteLine("Number of C's = " + numberC + " Which is " + percentC.ToString("0.00") + "% of the total grades.");
+            Console.WriteLine();
+            Console.WriteLine("Number of D's = " + numberD + " Which is " + percentD.ToString("0.00") + "% of the total grades.");
+            Console.WriteLine();
+            Console.WriteLine("Number of F's = " + numberF + " Which is " + percentF.ToString("0.00") + "% of the total grades.");
+            Console.WriteLine();
+
+        }
+    }
+}
