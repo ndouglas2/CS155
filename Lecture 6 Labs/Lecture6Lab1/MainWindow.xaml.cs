f615aed491500edcdabcb796e7ab5458ac38732d
Nate Douglas <nate.douglas2@gmail.com>
2020-10-12 14:23:26 -0700

    Completed Lecture 6 Labs

---
+++ Lecture 6 Labs/Lecture6Lab1/MainWindow.xaml.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Lecture6Lab1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            /*
+             * Value 0 = +
+             * Value 1 = -
+             * Value 2 = *
+             * Value 3 = /
+             * Value 4 = %
+             */ 
+            try
+            {
+                int operation = operandlist.SelectedIndex;
+                decimal val1 = Convert.ToDecimal(val1txtbx.Text);
+                decimal val2 = Convert.ToDecimal(val2txtbx.Text);
+                decimal result = 0;
+
+                switch (operation)
+                {
+                    case 0:
+                        result = val1 + val2;
+                        resultTxtbx.Text = result.ToString();
+                        break;
+                    case 1:
+                        result = val1 - val2;
+                        resultTxtbx.Text = result.ToString();
+                        break;
+                    case 2:
+                        result = val1 * val2;
+                        resultTxtbx.Text = result.ToString();
+                        break;
+                    case 3:
+                        result = val1 / val2;
+                        resultTxtbx.Text = result.ToString();
+                        break;
+                    case 4:
+                        result = val1 % val2;
+                        resultTxtbx.Text = result.ToString();
+                        break;
+                    default:
+                        result = 0;
+                        MessageBox.Show("No Operation was selected");
+                        break;
+                }
+            }
+            catch (DivideByZeroException)
+            {
+                MessageBox.Show("Error: Divide by 0 does not compute.");
+            }
+        }
+    }
+}
---
+++ Lecture 6 Labs/Lecture6Lab2/Program.cs
@@ -0,0 +1,114 @@
+﻿using System;
+
+namespace Lecture6Lab2
+{
+    public class Fraction
+    {
+        private int numerator = 0;
+        private int denominator = 0;
+        private double result = 0.0;
+
+        //constructor
+        public Fraction(int numerator, int denominator, double result)
+        {
+            this.numerator = numerator;
+            this.denominator = denominator;
+            this.result = result;
+        }
+
+        public Fraction()
+        {
+
+        }
+
+        //Setters and Getters for numerator and denominator
+        public void SetNumerator(int numerator)
+        {
+            this.numerator = numerator;
+        }
+
+        public int GetNumerator()
+        {
+            return numerator;
+        }
+
+        public void SetDenominator(int denominator)
+        {
+            this.denominator = denominator;
+        }
+
+        public int GetDenominator()
+        {
+            return denominator;
+        }
+
+        //Other Methods
+        public void Division(int numerator, int denominator)
+        {
+            result = numerator / (double)denominator;
+            Console.WriteLine("The fraction " + numerator + "/" + denominator + " equals " + result);
+        }
+
+        public int GCD(int a, int b)
+        {
+            while(a != b)
+            {
+                if(a < b)
+                {
+                    b = b - a;
+                }
+                else
+                {
+                    a = a - b;
+                }
+            }
+            return a;
+        }
+        public int LowestTerms(int a, int b)
+        {
+            int remainder = a;
+            int quotient = 0;
+
+            while (remainder >= b)
+            {
+                remainder = remainder - b;
+                quotient++;
+            }
+            return (quotient);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter the numerator of a fraction:");
+            Fraction original = new Fraction();
+            original.SetNumerator(Convert.ToInt32(Console.ReadLine()));
+
+            Console.WriteLine("Now Please enter the denominator: ");
+            original.SetDenominator(Convert.ToInt32(Console.ReadLine()));
+
+            original.Division(original.GetNumerator(), original.GetDenominator());
+
+            int reducedNumerator = 0;
+            int reducedDenominator = 0;
+
+            int divisor = original.GCD(original.GetNumerator(), original.GetDenominator());
+
+            if (divisor != 1)
+            {
+                reducedNumerator = original.LowestTerms(original.GetNumerator(), divisor);
+                reducedDenominator = original.LowestTerms(original.GetDenominator(), divisor);
+                Console.WriteLine("Your fraction can be reduced to " + reducedNumerator + "/" + reducedDenominator);
+            }
+            else
+            {
+                Console.WriteLine("Your fraction cannot be reduced.");
+            }
+
+         
+        }
+
+    }   
+}
