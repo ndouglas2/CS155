ad17c52627a4df8e2eb4c7bade763706b2897c1d
Nate Douglas <nate.douglas2@gmail.com>
2020-10-26 13:41:41 -0700

    Finished Lab 9

---
+++ Lecture 9 Labs/Lab1/Payment.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace Lab1
+{
+    class Payment
+    {
+        //Instance Variables
+        private double _paymentAmount;
+
+        //Constructors
+        public Payment()
+        {
+            _paymentAmount = 0;
+        }
+        public Payment(double p)
+        {
+            _paymentAmount = p;
+        }
+
+        //Getters and Setters
+        public double PaymentAmount { get; set; }
+
+        //Methods
+        public void PaymentDetais()
+        {
+            Console.WriteLine("The total payment is: $" + _paymentAmount);
+        }
+    }
+
+    class CashPayment : Payment
+    {
+        //Instance Variables
+
+        //Constructors
+        public CashPayment()
+        {
+            PaymentAmount = 0;
+        }
+        public CashPayment(double p)
+        {
+            PaymentAmount = p;
+        }
+        //Getters and Setters
+
+        //Methods
+        public void PaymentDetails()
+        {
+            Console.WriteLine("Payment Type: Cash");
+            Console.WriteLine("The total payment is: $" + PaymentAmount);
+        }
+    }
+
+    class CreditCardPayment : Payment
+    {
+        //Instance Variables
+        private string _cardName;
+        private string _cardNumber;
+        //Constructors
+        public CreditCardPayment()
+        {
+            PaymentAmount = 0;
+            CardName = "None";
+            CardNumber = "0000000";
+        }
+        public CreditCardPayment(double p, string n, string num)
+        {
+            PaymentAmount = p;
+            CardName = n;
+            CardNumber = num;
+        }
+        //Getters and Setters
+        public string CardName { get; set; }
+        public string CardNumber { get; set; }
+        //Methods
+        public void PaymentDetails()
+        {
+            Console.WriteLine("Payment Type: Credit Card");
+            Console.WriteLine("Card Name: " + CardName);
+            Console.WriteLine("Card Number: " + CardNumber);
+            Console.WriteLine("The total payment is: $" + PaymentAmount);
+        }
+
+    }
+}
---
+++ Lecture 9 Labs/Lab1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cash Payments
+            Console.WriteLine("Cash Payment 1 using blank constructor and Properties to set the Payment Amount:");
+            CashPayment payment1 = new CashPayment();
+            payment1.PaymentAmount = 999;
+            payment1.PaymentDetails();
+            Console.WriteLine("");
+
+            Console.WriteLine("Cash Payment 2 using Value pass through constructor to set values:");
+            CashPayment payment2 = new CashPayment(300);
+            payment2.PaymentDetails();
+            Console.WriteLine("");
+
+            //Credit Payments
+            Console.WriteLine("Credit Card Payment 1 using blank constructor and Properties to set values:");
+            CreditCardPayment credit1 = new CreditCardPayment();
+            credit1.PaymentAmount = 670;
+            credit1.CardName = "Jonathan Sims";
+            credit1.CardNumber = "8894561";
+            credit1.PaymentDetails();
+            Console.WriteLine("");
+
+            Console.WriteLine("Credit Card Payment 2 using Value pass through constructor to set values:");
+            CreditCardPayment credit2 = new CreditCardPayment(450, "Quincey Jones", "1236547");
+            credit2.PaymentDetails();
+            Console.ReadLine();
+        }
+    }
+}
