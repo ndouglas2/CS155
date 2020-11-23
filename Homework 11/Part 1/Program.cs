fcb7c6005bf58638b280a4a103bf7ab2ed5f834f
Nate Douglas <nate.douglas2@gmail.com>
2020-11-23 13:31:35 -0800

    Completed Homework 11.

---
+++ Homework 11/Part 1/Program.cs
@@ -0,0 +1,78 @@
+﻿/// Chapter No. 11		Exercise No. 1
+/// File Name: Homework11Part1.cs
+/// @author: Nate Douglas
+/// Date:  November 23, 2020
+///
+/// Problem Statement: Create a student class and main method that allows reading names and scores from a file, computing average and median, and then writing to a new file
+/// 
+/// 
+/// Overall Plan:
+/// 1) Create a Student class that extends IComparable with the necessary instance variables, constructors, etc.
+/// 2) Create a CompareTo method that allows for list sorting within the Student Class
+/// 3) Create mehtods for calculating average and median
+/// 4) Open the file studentInfo.txt, and take the information and store it as objects in a list called sudents
+/// 5) Open the file to write to StudentStats.txt, and write the average and median of the student scores. 
+/// 
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int total = 0;
+            List<Student> students = new List<Student>();
+
+            try
+            {
+                StreamReader sr = new StreamReader("studentInfo.txt");
+
+                while((line = sr.ReadLine()) != null)
+                {
+                    string[] lineData = line.Split(' ');
+                    var fname = lineData[0];
+                    var lname = lineData[1];
+                    var score = Int32.Parse(lineData[2]); 
+                    var student = new Student(fname, lname, score);
+                    students.Add(student);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            students.Sort();
+
+            foreach(var s in students)
+            {
+                total += s.Score;
+            }
+
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter("StudentStats.txt"))
+            {
+                try
+                {
+                    sw.WriteLine("The average of all scores is: " + Student.CalcAvg(total, students));
+                    sw.WriteLine("The median of all scores is: " + Student.CalcMedian(students));
+                    Console.WriteLine("Successfully Wrote to StudentStats.txt");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Exception: " + e.Message);
+                }
+            }
+
+            
+        }
+    }
+}
---
+++ Homework 11/Part 1/Student.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace Part_1
+{
+    class Student : IComparable<Student>
+    {
+        //Instance Variables
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Score { get; set; }
+
+        //Constructors
+        public Student()
+        {
+
+        }
+        public Student(string f, string l, int s)
+        {
+            FirstName = f;
+            LastName = l;
+            Score = s;
+        }
+
+        //Methods
+        public static int CalcAvg(int t, List<Student> s)
+        {
+            int total = t;
+            int avg = total / s.Count;
+
+            return avg;
+        }
+
+        public static int CalcMedian(List<Student> s)
+        {
+            double position = 0;
+            double median = 0.0;
+            
+            if(s.Count % 2 == 0)
+            {
+                median = ((s[(s.Count / 2) - 1].Score + s[(s.Count / 2)].Score) / 2);
+                return Convert.ToInt32(median);
+            }
+            else if (s.Count % 2 == 1)
+            {
+                position = Math.Ceiling((double)s.Count / 2) - 1;
+            }
+
+            return s[Convert.ToInt32(position)].Score;
+        }
+        public int CompareTo(Student obj)
+        {
+            if(this.Score == obj.Score)
+            {
+                return this.FirstName.CompareTo(obj.FirstName);
+            }
+
+            return obj.Score.CompareTo(this.Score);
+        }
+    }
+}
