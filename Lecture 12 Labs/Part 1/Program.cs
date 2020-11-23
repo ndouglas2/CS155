99981708405e99c575d884556a09fdade2f87e2c
Nate Douglas <nate.douglas2@gmail.com>
2020-11-22 17:23:07 -0800

    Finished Lab 12

---
+++ Lecture 12 Labs/Part 1/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            int choice = 0;
+            string name = " ";
+            Dictionary<string, string> boyNames = new Dictionary<string, string>();
+            Dictionary<string, string> girlNames = new Dictionary<string, string>();
+            try
+            {
+
+                StreamReader sr = new StreamReader("boynames-1.txt");
+
+                while((line = sr.ReadLine()) != null)
+                {
+                    string[] lineData = line.Split(' ');
+                    boyNames.Add(lineData[0].ToUpper(), lineData[1]);
+                }
+
+                StreamReader sr2 = new StreamReader("girlnames-1.txt");
+
+                while ((line = sr2.ReadLine()) != null)
+                {
+                    string[] lineData = line.Split(' ');
+                    girlNames.Add(lineData[0].ToUpper(), lineData[1]);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Exception: " + e.Message);
+            }
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Welcome to the Baby Names Ranking System");
+                Console.WriteLine("Please make your selection from the list below: ");
+                Console.WriteLine("1. Search Boy Names");
+                Console.WriteLine("2. Search Girl Names");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine();
+                Console.Write("Your selection: ");
+                choice = Int32.Parse(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.Clear();
+                        Console.WriteLine("BOY NAMES");
+                        Console.WriteLine("Please enter the name you wish to search for: ");
+                        name = Console.ReadLine().ToUpper();
+
+                        if (boyNames.ContainsKey(name))
+                        {
+                            string births = boyNames[name];
+                            Console.WriteLine("Name: {0} Number of Births: {1}", name, births);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Name could not be found in Registry.");
+                        }
+
+                        Console.WriteLine("Press enter to return to the main menu.");
+                        Console.ReadLine();
+                        break;
+
+                    case 2:
+                        Console.Clear();
+                        Console.WriteLine("GIRL NAMES");
+                        Console.WriteLine("Please enter the name you wish to search for: ");
+                        name = Console.ReadLine().ToUpper();
+
+                        if (girlNames.ContainsKey(name))
+                        {
+                            string births = girlNames[name];
+                            Console.WriteLine("Name: {0} Number of Births: {1}", name, births);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Name could not be found in Registry.");
+                        }
+
+                        Console.WriteLine("Press enter to return to the main menu.");
+                        Console.ReadLine();
+                        break;
+
+                    case 3:
+                        Environment.Exit(1);
+                        break;
+
+                    default:
+                        Console.WriteLine("That is not a valid option.");
+                        break;
+
+                }
+
+            } while (choice != 3) ;
+
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ Lecture 12 Labs/Part 2/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.IO;
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Optional Parameters
+            Student("Nate", "Douglas", 23, "Computer Science");
+            Student("John", "C. Reily");
+            Student("Stuart", "Little", 24);
+            Console.WriteLine();
+
+            //Named Parameters
+            Console.WriteLine(AddNumber(12, 13));
+            Console.WriteLine(AddNumber(secondNum: 13, firstNum: 12));
+            Console.WriteLine();
+
+            //Variable # of Parameters
+            Console.WriteLine(MultiplyNumber(5, 6));
+            Console.WriteLine(MultiplyNumber(5, 6, 7));
+            Console.WriteLine(MultiplyNumber(5, 6, 7, 8));
+        }
+
+        //Method for Optional Parameters
+        public static void Student(string fname, string lname, int age = 20, string major = "Political Science")
+        {
+            Console.WriteLine("First Name: {0} Last Name: {1} Age: {2} Major: {3}", fname, lname, age, major);
+        }
+
+        //Method for Named Parameters
+        public static int AddNumber(int firstNum, int secondNum)
+        {
+            return firstNum + secondNum;
+        }
+
+        //Method for Variable # of Parameters
+        public static int MultiplyNumber(params int[] num)
+        {
+            int result = 1;
+
+            foreach(int i in num)
+            {
+                result *= i;
+            }
+
+            return result;
+        }
+    }
+}
