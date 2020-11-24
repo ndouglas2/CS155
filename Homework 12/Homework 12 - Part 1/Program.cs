311f1e43b26967639a2a6bf37d62b8160e70cdc2
Nate Douglas <nate.douglas2@gmail.com>
2020-11-23 18:05:11 -0800

    Finished Lab 13 (in class)

---
+++ Homework 12/Homework 12 - Part 1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Homework_12___Part_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Lecture 13 Labs/Lecture 13 Labs Part 1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Lecture_13_Labs_Part_1
+{
+    class Rectangle
+    {
+        static void Main(string[] args)
+        {
+            Rectangle rect = new Rectangle();
+            mydelegate rectdele = new mydelegate(rect.area);
+
+            //Alternative way:
+            //mydelegate rectdele = rect.area;
+
+            rectdele += new mydelegate(rect.perimeter);
+
+            //Alternative way:
+            //rectdele += rect.perimiter
+
+            rectdele.Invoke(6.0, 4.5);
+            Console.WriteLine();
+            rectdele.Invoke(15.3, 11.4);
+        }
+        public delegate void mydelegate(double height, double width);
+        public void area(double height, double width)
+        {
+            Console.WriteLine("The Area is: " + (width * height));
+        }
+
+        public void perimeter(double height, double width)
+        {
+            Console.WriteLine("The Perimeter is: " + (2*(width * height)));
+        }
+    }
+
+}
