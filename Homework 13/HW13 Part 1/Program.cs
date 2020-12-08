7113bd8b1e01b258f2d563fef4cb9092d36fd7ae
Nate Douglas <nate.douglas2@gmail.com>
2020-12-07 18:38:22 -0800

    Homework 13 - In Class example

---
+++ Homework 13/HW13 Part 1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //write into SQL database using entity framework
+            //Console.WriteLine("Enter employee first name ");
+            //string fname = Console.ReadLine();
+            //Console.WriteLine("Enter employee last name ");
+            //string lname = Console.ReadLine();
+            //Console.WriteLine("Enter employee gender ");
+            //string gender = Console.ReadLine();
+            //Console.WriteLine("Enter employee salary ");
+            //int salary = Int32.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter department ID ");
+            //int deptId = Int32.Parse(Console.ReadLine());
+
+            //using (var context = new CS155Entities())
+            //{
+            //    var employee = new Employee()
+            //    {
+            //        FirstName = fname,
+            //        LastName = lname,
+            //        Gender = gender,
+            //        Salary = salary,
+            //        DepartmentId = deptId
+
+            //    };
+            //    context.Employees.Add(employee);
+            //    context.SaveChanges(); 
+            //}
+
+            //Reading from SQL database using entity framework
+            using (var context = new CS155Entities())
+            {
+                foreach (var emp in context.Employees)
+                {
+                    Console.Write(" " + emp.FirstName + "    " + emp.LastName + "   " + emp.Gender + "  " + emp.Salary + "  " + emp.Department.Name);
+                    Console.WriteLine();
+
+                };
+                Console.ReadKey();
+            }
+
+        }
+    }
+}
